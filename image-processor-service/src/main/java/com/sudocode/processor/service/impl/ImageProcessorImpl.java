b7348ad202821e95eee828eb352511b05f11c99f
Jomer Pangilinan <ace@ace.com>
2016-03-19 02:17:06 -0400

    Stripped codes for library usage

--- image-processor-service/src/main/java/com/sudocode/processor/service/impl/ImageProcessorImpl.java
+++ image-processor-service/src/main/java/com/sudocode/processor/service/impl/ImageProcessorImpl.java
@@ -1,36 +1,27 @@
 package com.sudocode.processor.service.impl;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;;
 
 import com.sudocode.image.processor.service.ImageProcessorService;
 import com.sudocode.processor.utils.Logger;
 
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+
+;
 
 /**
  * Created by ace on 3/5/16.
@@ -39,7 +30,7 @@ public class ImageProcessorImpl implements ImageProcessorService {
 
     private static final Logger logger = Logger.getLogger(ImageProcessorImpl.class);
     private CvCameraViewFrame inputFrame;
-    private Mat gradx,grady,absgradx,absgrady;
+    private Mat gradx, grady, absgradx, absgrady;
     private Mat mSource;
     //private Mat mDest;
     private Bitmap mBitmap;
@@ -49,11 +40,11 @@ public class ImageProcessorImpl implements ImageProcessorService {
         return instance;
     }
 
-   public ImageProcessorImpl() {
+    public ImageProcessorImpl() {
         if (!OpenCVLoader.initDebug()) {
         } else {
         }
-   }
+    }
 
     public static void init() {
         if (instance == null) {
